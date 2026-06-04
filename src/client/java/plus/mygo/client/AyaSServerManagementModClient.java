77925fab8ee3100727fb14de45ba8988968e9ab4
peter2500zz <2797880090@qq.com>
2026-06-04 23:37:45 +0800

    Initial project setup from fabric template

---
+++ src/client/java/plus/mygo/client/AyaSServerManagementModClient.java
@@ -0,0 +1,10 @@
+package plus.mygo.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AyaSServerManagementModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
